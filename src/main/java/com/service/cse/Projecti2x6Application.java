f73370dfce9833c91c611b24e3e63c80e5ed9aa2
paas <paas@cse-codegen-backend-1310641886-hlqql>
2018-10-11 03:11:20 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/cse/Projecti2x6Application.java
@@ -0,0 +1,11 @@
+package com.service.cse;
+
+import org.apache.servicecomb.foundation.common.utils.BeanUtils;
+import org.apache.servicecomb.foundation.common.utils.Log4jUtils;
+
+public class Projecti2x6Application {
+    public static void main(String[] args) throws Exception {
+        Log4jUtils.init();
+        BeanUtils.init();
+    }
+}
